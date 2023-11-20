276b3d187f2fffafc2a3e15df56b666a8bae0aaf
Qwerty-pixl <gulanda727@gmail.com>
2023-11-20 21:37:20 +0600

    change js

--- js/core.js
+++ js/core.js
@@ -1,3 +1,7 @@
 document.querySelector('.js-btn').addEventListener('click', function () { 
     document.querySelector('.js-btn').classList.add('active')
+    document.querySelector('.js-btn').computedStyleMap.height = "200px"
+    document.querySelector('.js-btn').textContent = "Текст кнопки изменен"
+     
+    let newhr = document.createElement('<p>Paragraph</p>');
 })
